5b74da457c7a3c59a5e8df4296a65d404731bb5a
Anmol Desai <anmoldesai4@gmail.com>
2020-04-03 19:45:10 +1100

    setup handling and setting fields

--- src/containers/Wrapper/index.js
+++ src/containers/Wrapper/index.js
@@ -2,8 +2,24 @@ import React from "react";
 import { WrapperElement } from "./index.style";
 import DetailsForm from "../DetailsForm";
 import HCard from "../HCard";
+import { useFormFields } from "../../utils";
 
 export default function Wrapper(props) {
+  const [fields, handleFieldChange] = useFormFields({
+    givenName: "",
+    surname: "",
+    email: "",
+    email: "",
+    phone: "",
+    houseName: "",
+    street: "",
+    suburrb: "",
+    state: "",
+    postcode: "",
+    country: "",
+    avatar: "",
+  });
+
   return (
     <WrapperElement>
       <DetailsForm />
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12,3 +12,19 @@ export const respondTo = Object.keys(breakpoints).reduce(
   },
   {}
 );
+
+import { useState } from "react";
+
+export function useFormFields(initialState) {
+  const [fields, setValues] = useState(initialState);
+
+  return [
+    fields,
+    function (event) {
+      setValues({
+        ...fields,
+        [event.target.id]: event.target.value,
+      });
+    },
+  ];
+}
