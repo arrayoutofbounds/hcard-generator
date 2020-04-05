a98c8c76e424351a0b4a044e5156ad0e5ef2c0da
Anmol Desai <anmoldesai4@gmail.com>
2020-04-05 17:20:15 +1000

    fix build

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 /* istanbul ignore file */
 import React from "react";
-import { hydrate, render } from "react-dom";
+// import { render } from "react-dom";
+import { render } from "react-snapshot";
 import "./index.css";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 
 const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement
-  );
-} else {
-  render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement
-  );
-}
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
